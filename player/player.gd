a1941dd644ee8e9b0e5ce68d3d4a4b78c425bc7a
Gen Ichino <zenoplex@gmail.com>
2024-01-05 01:45:44 +0900

    feat: Add player input handling

--- player/player.gd
+++ player/player.gd
@@ -6,21 +6,46 @@ class_name Player
 var gravity := 750
 var run_speed := 150
 var jump_speed := -300
-enum State { IDLE, RUNNING, JUMPING, HURT, DEAD}
+enum State { IDLE, RUN, JUMP, HURT, DEAD}
 var state := State.IDLE
 
 func _ready():
-		change_state(State.IDLE)
+	_change_state(State.IDLE)
 
-func change_state(_state: State) -> void:
+func _change_state(_state: State) -> void:
 	match _state:
 		State.IDLE:
 			$AnimationPlayer.play("idle")
-		State.RUNNING:
+		State.RUN:
 			$AnimationPlayer.play("run")
-		State.JUMPING:
+		State.JUMP:
 			$AnimationPlayer.play("jump_up")
 		State.HURT:
 			$AnimationPlayer.play("hurt")
 		State.DEAD:
 			hide()
+
+func _get_input() -> void:
+	var is_right_pressed := Input.is_action_pressed("right")
+	var is_left_pressed := Input.is_action_pressed("left")
+	var is_jump_pressed := Input.is_action_just_pressed("jump")
+
+	if is_right_pressed:
+		velocity.x += run_speed
+	if is_left_pressed:
+		velocity.x -= run_speed
+	# Only allow jump if player is touching the floor
+	if is_jump_pressed and is_on_floor():
+		_change_state(State.JUMP)
+		velocity.y = jump_speed
+	
+	if velocity.x != 0:
+		_change_state(State.RUN)
+	elif velocity.x == 0:
+		_change_state(State.IDLE)
+
+func _physics_process(_delta: float) -> void:
+	_get_input()
+	velocity.y += gravity * _delta
+	move_and_slide()
+	
