956c01cbec674eadba5f1acd1e2637f74329239d
Gen Ichino <zenoplex@gmail.com>
2024-01-06 19:07:48 +0900

    feat: Add item textures and initialize item sprite

--- items/Item.gd
+++ items/Item.gd
@@ -1,2 +1,21 @@
 extends Area2D
 class_name Item
+
+const gem_texture = "./gem.png"
+const cherry_texture = "./cherry.png"
+enum ItemType { GEM, CHERRY }
+
+@onready var sprite: Sprite2D = get_node("Sprite2D")
+
+func _init(_type: ItemType, _position: Vector2) -> void:
+  var texture: Resource
+  match _type:
+    ItemType.GEM:
+      texture = load(gem_texture)
+    ItemType.CHERRY:
+      texture = load(cherry_texture)
+    _:
+      assert(false, "Invalid item type")
+  
+  sprite.texture = texture
+  position = _position
