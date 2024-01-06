28d1fb57bf8f144075ef65fccb8c1e2ebf7f7de5
Gen Ichino <zenoplex@gmail.com>
2024-01-06 23:57:54 +0900

    refactor: Update item texture constants

--- items/Item.gd
+++ items/Item.gd
@@ -4,8 +4,8 @@ class_name Item
 ## Emits on body entered
 signal picked_up
 
-const gem_texture = "./gem.png"
-const cherry_texture = "./cherry.png"
+const GEM_TEXTURE = "./gem.png"
+const CHERRY_TEXTURE = "./cherry.png"
 enum ItemType { GEM, CHERRY }
 
 @onready var sprite: Sprite2D = get_node("Sprite2D")
@@ -14,9 +14,9 @@ func init(_type: ItemType, _position: Vector2) -> void:
   var texture: Resource
   match _type:
     ItemType.GEM:
-      texture = load(gem_texture)
+      texture = load(GEM_TEXTURE)
     ItemType.CHERRY:
-      texture = load(cherry_texture)
+      texture = load(CHERRY_TEXTURE)
     _:
       assert(false, "Invalid item type")
   
