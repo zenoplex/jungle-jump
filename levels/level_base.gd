ff00af27e59e5726851ac4e5033766c726ee97a4
Gen Ichino <zenoplex@gmail.com>
2024-01-10 01:02:36 +0900

    feat: Add item spawning functionality to level_base.gd

--- levels/level_base.gd
+++ levels/level_base.gd
@@ -4,10 +4,14 @@ class_name LevelBase
 @onready var player: Player = get_node("Player")
 @onready var spawn_marker: Marker2D = get_node("SpawnMarker2D")
 @onready var world: TileMap = get_node("World")
+@onready var items: TileMap = get_node("Items")
+
+@export var item_scene: PackedScene
 
 func _ready():
 	player.reset(spawn_marker.position)
 	_set_camera()
+	_spawn_items()
 
 func _set_camera() -> void:
 	var map_size := world.get_used_rect()
@@ -18,3 +22,26 @@ func _set_camera() -> void:
 	var right := (map_size.end.x + buffer_size) * cell_size.x
 
 	player.set_camera_limit(left, right)
+
+func _spawn_items() -> void:
+	var item_cells := items.get_used_cells(0)
+	for cell in item_cells:
+		var data := items.get_cell_tile_data(0, cell)
+		var type: Variant = data.get_custom_data("type")
+
+		match type:
+			"gem":
+				_add_item(Item.ItemType.GEM, cell)
+			"cherry":
+				_add_item(Item.ItemType.CHERRY, cell)
+			_:
+				pass
+
+func _add_item(type: Item.ItemType, cell: Vector2i) -> void:
+	var item := item_scene.instantiate() as Item
+	add_child(item)
+	item.init(type, items.map_to_local(cell))
+	item.picked_up.connect(self._on_item_picked_up)
+	
+func _on_item_picked_up() -> void:
+	print("Item picked up")
