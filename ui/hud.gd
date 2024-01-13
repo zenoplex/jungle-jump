1efde549398f1da1d356d9d68ca87bcbeb355fb3
Gen Ichino <zenoplex@gmail.com>
2024-01-13 23:40:52 +0900

    feat: Add HUD class and update HUD scene

---
+++ ui/hud.gd
@@ -0,0 +1,17 @@
+extends MarginContainer
+class_name HUD
+
+@onready var score_label: Label = get_node("HBoxContainer/ScoreLabel")
+@onready var life_counter: HBoxContainer = get_node("HBoxContainer/LifeCounter")
+
+func _set_life(_life: int) -> void:
+	# NOTE: Max displayable life is 5 due to UI restriction.
+	# Maybe append depending on setting first.
+	var children := life_counter.get_children()
+	for i in life_counter.get_child_count():
+		var child := children[i]
+		if child is TextureRect:
+			child.visible = i < _life
+	
+func _set_score(_score: int) -> void:
+	score_label.text = str(_score)
