8137c108a541953bd68f13d7c2bb1c37e62a744d
Gen Ichino <zenoplex@gmail.com>
2024-01-14 21:20:28 +0900

    feat: Add level loading functionality

--- main.gd
+++ main.gd
@@ -2,8 +2,8 @@ extends Node
 class_name Main
 
 func _ready() -> void:
-	pass
-
-
-func _process(_delta: float) -> void:
-	pass
+	var level_num := str(GameState.current_level).pad_zeros(2)
+	var path := "res://levels/1level_%s.tscn" % level_num
+	var resource := load(path)
+	var level = resource.instantiate()
+	add_child(level)
