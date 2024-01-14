442ec70dc3686dbc43adaa4705ade69e3857f6a8
Gen Ichino <zenoplex@gmail.com>
2024-01-14 21:20:40 +0900

    feat: Add game_state.gd and update project.godot

---
+++ game_state.gd
@@ -0,0 +1,16 @@
+extends Node
+
+var num_levels := 2
+var current_level := 1
+var game_scene := "res://main.tscn"
+var title_scene := "res://ui/title_scene.tscn"
+
+func restart() -> void:
+	current_level = 0
+	get_tree().change_scene_to_file(title_scene)
+
+func next_level() -> void:
+	current_level += 1
+	if current_level > num_levels:
+		current_level = 1
+	get_tree().change_scene_to_file(game_scene)
