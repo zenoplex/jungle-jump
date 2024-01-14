8784f89231d7957931c2eb91be996ba868e13905
Gen Ichino <zenoplex@gmail.com>
2024-01-14 21:22:50 +0900

    fix: Update level file path in _ready function

--- main.gd
+++ main.gd
@@ -3,7 +3,7 @@ class_name Main
 
 func _ready() -> void:
 	var level_num := str(GameState.current_level).pad_zeros(2)
-	var path := "res://levels/1level_%s.tscn" % level_num
+	var path := "res://levels/level_%s.tscn" % level_num
 	var resource := load(path)
 	var level = resource.instantiate()
 	add_child(level)
