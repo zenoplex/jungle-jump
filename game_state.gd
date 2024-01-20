9b75c0ae56e8d2d98ac776d58e91566b698e3b6c
Gen Ichino <zenoplex@gmail.com>
2024-01-20 18:31:51 +0900

    fix: Fix scene change issue

--- game_state.gd
+++ game_state.gd
@@ -7,10 +7,12 @@ var title_scene := "res://ui/title.tscn"
 
 func restart() -> void:
 	current_level = 0
-	get_tree().change_scene_to_file(title_scene)
+	# https://github.com/godotengine/godot/issues/85852
+	get_tree().change_scene_to_file.bind(title_scene).call_deferred()
 
 func next_level() -> void:
 	current_level += 1
 	if current_level > num_levels:
 		current_level = 1
-	get_tree().change_scene_to_file(game_scene)
+	# https://github.com/godotengine/godot/issues/85852
+	get_tree().change_scene_to_file.bind(game_scene).call_deferred()
