de257c9861716908764fbeb9c1430b55f8ed8d35
Gen Ichino <zenoplex@gmail.com>
2024-01-22 16:18:01 +0900

    refactor: export offset and duration

--- levels/moving_platform.gd
+++ levels/moving_platform.gd
@@ -3,11 +3,11 @@ class_name  MovingPlatform
 
 @onready var tileMap: TileMap = get_node("TileMap")
 
-const OFFSET := Vector2(320, 0)
-const DURATION := 10.0
+@export var offset := Vector2(320, 0)
+@export var duration := 10.0
 
 func _ready() -> void:
 	var tween := create_tween().set_process_mode(Tween.TWEEN_PROCESS_PHYSICS)
 	tween.set_loops().set_parallel(false)
-	tween.tween_property(tileMap, "position", OFFSET, DURATION * 0.5).from_current()
-	tween.tween_property(tileMap, "position", Vector2.ZERO, DURATION * 0.5)
+	tween.tween_property(tileMap, "position", offset, duration * 0.5).from_current()
+	tween.tween_property(tileMap, "position", Vector2.ZERO, duration * 0.5)
